b04d6289f7a530dd800221fb1780866e86cb8e2a
Reza Bagus Saputra <103319101+RezaBagusS@users.noreply.github.com>
2023-10-17 12:42:01 +0700

    Initial Setup ReactViteJS + TailwindCSS + Flowbite + react-router-dom + framer-motion

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+
+  return (
+    <>
+      <div className='cust-outer-container text-4xl font-bold'>
+        <div className="cust-container bg-slate-600">
+          <h1>HOME</h1>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
