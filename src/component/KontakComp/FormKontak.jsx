802749013fd1beef52f556b7dacd6c13afa54a21
Reza Bagus Saputra <103319101+RezaBagusS@users.noreply.github.com>
2023-10-28 00:03:01 +0700

    adding func sendEmail with Email.js

--- src/component/KontakComp/FormKontak.jsx
+++ src/component/KontakComp/FormKontak.jsx
@@ -1,7 +1,9 @@
+import { useRef } from "react";
 import ballPattern from "../../assets/ball-pattern.svg";
 import propType from "prop-types";
+import emailjs from "@emailjs/browser";
 
-const InputField = ({ placeHolder, id, type, labelName }) => {
+const InputField = ({ placeHolder, name, id, type, labelName }) => {
   return (
     <div className="flex flex-col gap-3">
       <label className="text-cust-gray-500 text-lg font-semibold">
@@ -12,14 +14,18 @@ const InputField = ({ placeHolder, id, type, labelName }) => {
           className="text-cust-teal-500 text-base font-medium rounded-lg resize-y"
           id={id}
           placeholder={placeHolder}
+          name={name}
           cols="30"
           rows="3"
+          autoComplete="off"
         ></textarea>
       ) : (
         <input
           className="text-cust-teal-500 text-sm sm:text-base font-medium rounded-lg"
           placeholder={placeHolder}
+          name={name}
           type={type}
+          autoComplete="off"
           id={id}
         />
       )}
@@ -28,30 +34,61 @@ const InputField = ({ placeHolder, id, type, labelName }) => {
 };
 
 const FormKontak = () => {
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_hziqrzf",
+        "template_2z5s64e",
+        form.current,
+        "C6ahhuh0WSjR1G4KU"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+      e.target.reset();
+  };
+
   return (
     <div className="relative cust-container flex justify-center py-5">
-      <form className="w-10/12 md:w-8/12 flex flex-col gap-5">
+      <form
+        ref={form}
+        onSubmit={sendEmail}
+        className="w-10/12 md:w-8/12 flex flex-col gap-5"
+      >
         <InputField
           placeHolder={"Isi nama lengkap kamu."}
+          name="user_name"
           id="nama"
           type="text"
           labelName={"Nama Lengkap"}
         />
         <InputField
           placeHolder={"Isi email kamu."}
+          name="user_email"
           id="email"
           type="email"
           labelName={"Alamat Email"}
         />
         <InputField
           placeHolder={"Isi pesan yang mau kamu sampaikan."}
+          name="message"
           id="pesan"
           type="text"
           labelName={"Apa Pesanmu?"}
         />
         <button
           type="submit"
-          className="w-fit ms-auto bg-cust-teal-500 hover:bg-cust-teal-500/70 text-white text-xs sm:text-sm font-semibold px-6 py-2 rounded-lg drop-shadow-[0px_3px_3px_rgba(0,0,0,0.2)] transition-all duration-150"
+          className="w-fit cursor-pointer ms-auto bg-cust-teal-500 hover:bg-cust-teal-500/70 text-white text-xs sm:text-sm font-semibold px-6 py-2 rounded-lg drop-shadow-[0px_3px_3px_rgba(0,0,0,0.2)] transition-all duration-150"
         >
           Kirim Pesan
         </button>
@@ -68,6 +105,7 @@ export default FormKontak;
 InputField.propTypes = {
   placeHolder: propType.string.isRequired,
   id: propType.string.isRequired,
+  name: propType.string.isRequired,
   type: propType.string.isRequired,
   labelName: propType.string.isRequired,
 };
